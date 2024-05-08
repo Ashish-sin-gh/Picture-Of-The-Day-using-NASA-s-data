09f931b37145c473033a0a8c582559fb136539c8
Ashish Kumar Singh <ashish159singh@gmail.com>
2024-05-09 00:12:04 +0530

    embedded video playback supported update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 document.querySelector('button').addEventListener('click', viewPic);
 
 function viewPic(){
-    const dateSelected = document.getElementById('dateInput').value;
-    const url = `https://api.nasa.gov/planetary/apod?api_key=UOu1hcfU9q7CnigBAghNmgtWt7Vixjp4lMXBxCoN&date=${dateSelected}`;
+const dateSelected = document.getElementById('dateInput').value;
+const url = `https://api.nasa.gov/planetary/apod?api_key=UOu1hcfU9q7CnigBAghNmgtWt7Vixjp4lMXBxCoN&date=${dateSelected}`;
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            document.querySelector('h3').innerHTML = `explanation: ${data.explanation}`;
-            document.querySelector('img').src = data.hdurl;
-            document.querySelector('img').alt = "there is no pic for today";
-        })
-        .catch(err => console.log(`error ${err}`));
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        const iframe = document.querySelector('iframe');
+        const image = document.querySelector('img');
+
+        document.querySelector('h3').innerHTML = `explanation: ${data.explanation}`;
+        if(data.media_type === 'image'){
+            iframe.style.display = "none"; 
+            if(image.style.display === "none"){
+            image.style.display = "block";
+            }
+            image.src = data.hdurl;
+            image.alt = "there is no pic for today";
+        }else if(data.media_type === 'video'){
+            image.style.display = "none";
+            if(iframe.style.display === "none"){
+                iframe.style.display = "block";
+            }
+            iframe.src = data.url;
+        }   
+    })
+    .catch(err => console.log(`error ${err}`));
 }
